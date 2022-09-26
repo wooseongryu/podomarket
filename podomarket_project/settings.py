730cfc784fac5f93809246a13b3230241f6059ef
us3754 <useong3754@gmail.com>
2022-09-26 20:20:40 +0900

    Allauth 기본 설정 models.py에서 유저 모델 생성 후 settings.py에 AUTH_USER_MODEL 설정 migrations와 migrate 슈퍼유저 생성 django-allauth설치 후 settings.py내용 작성과 urls.py내용 작성 EMAIL_BACKEND설정 후 migrate

--- podomarket_project/settings.py
+++ podomarket_project/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'podomarket',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +128,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth Settings
+
+AUTH_USER_MODEL = 'podomarket.User'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
