2e5bcfafd8ba1d7bbc691e5c448d5db32eb8c84f
us3754 <useong3754@gmail.com>
2022-09-27 22:18:21 +0900

    유효성 검사 settings.py에서 기본언어 한국어 변경 models.py에서 nickname 오류 메시지 작성 validators.py생성 models.py에서 유효성검사 추가 settings.py에서 AUTH_PASSWORD_VALIDATORS 수정 및 ACCOUNT_PASSWORD_INPUT_RENDER_VALUE 작성

--- podomarket/models.py
+++ podomarket/models.py
@@ -1,11 +1,25 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from podomarket.validators import validate_no_special_characters
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=15, unique=True, null=True)
-    kakao_id = models.CharField(max_length=20, null=True)
-    address = models.CharField(max_length=40, null=True)
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators = [validate_no_special_characters],
+        error_messages={"unique": "이미 사용중인 닉네임입니다."}
+    )
+    kakao_id = models.CharField(
+        max_length=20,
+        null=True,
+        validators = [validate_no_special_characters]
+    )
+    address = models.CharField(
+        max_length=40,
+        null=True,
+        validators = [validate_no_special_characters]
+    )
 
     def __str__(self):
         return self.email
