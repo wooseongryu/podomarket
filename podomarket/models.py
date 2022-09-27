3b5efe938d5c39ff926144339a783017b3d234d7
us3754 <useong3754@gmail.com>
2022-09-27 19:49:13 +0900

    필드 추가 models.py에 User모델 작성 추가 필드에는 null=True옵션 makmigrations와 migrate admin.py에 추가한 필드들이 어드민 페이지에도 나타나도록 작성 forms.py에서 폼 작성 settings.py에서 정의한 폼 클래스를 사용하도록 작성 index.html수정

--- podomarket/models.py
+++ podomarket/models.py
@@ -1,7 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    nickname = models.CharField(max_length=15, unique=True, null=True)
+    kakao_id = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=40, null=True)
+
     def __str__(self):
         return self.email
         
