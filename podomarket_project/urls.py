96b830e7989dbc066a50805ecce22bdeadccf88b
us3754 <useong3754@gmail.com>
2022-10-02 22:27:13 +0900

    urls수정 미디어 파일 처리 코드를 앱 urls에서 프로젝트 앱 urls로 이동

--- podomarket_project/urls.py
+++ podomarket_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from podomarket.views import CustomPasswordChangeView
 
 urlpatterns = [
@@ -33,3 +35,6 @@ urlpatterns = [
     ),
     path('', include('allauth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
