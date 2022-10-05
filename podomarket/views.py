0e40e5d9f8dabc11babd0ca32987176dfb0880f1
us3754 <useong3754@gmail.com>
2022-10-05 15:45:47 +0900

    수정/삭제 접근 제어 post_detail.html에서 글 작성자와 현재 유저가 일치할 때만 버튼이 나오도록 수정 views.py에서 url로 접근했을 때의 접근 제어 작성

--- podomarket/views.py
+++ podomarket/views.py
@@ -7,9 +7,12 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from braces.views import LoginRequiredMixin, UserPassesTestMixin
+from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 from .models import Post
 from .forms import PostCreateForm, PostUpdateForm
+from .functions import confirmation_required_redirect
 
 
 def index(request):
@@ -22,15 +25,18 @@ class IndexView(ListView):
     paginate_by = 8
     ordering = ["-dt_updated"]
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'podomarket/post_detail.html'
     pk_url_kwarg = 'post_id'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     form_class = PostCreateForm
     template_name = 'podomarket/post_form.html'
+
+    redirect_unauthenticated_users = True
+    raise_exception = confirmation_required_redirect
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -39,22 +45,37 @@ class PostCreateView(CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={"post_id": self.object.id})
 
-class PostUpdateView(UpdateView):
+    def test_func(self, user):
+        return EmailAddress.objects.filter(user=user, verified=True).exists()
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostUpdateForm
     template_name = 'podomarket/post_form.html'
     pk_url_kwarg = 'post_id'
 
+    raise_exception = True
+
     def get_success_url(self):
         return reverse('post-detail', kwargs={"post_id": self.object.id})
 
-class PostDeleteView(DeleteView):
+    def test_func(self, user):
+        post = self.get_object()
+        return post.author == user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'podomarket/post_confirm_delete.html'
     pk_url_kwarg = 'post_id'
 
+    raise_exception = True
+
     def get_success_url(self):
         return reverse('index')
+
+    def test_func(self, user):
+        post = self.get_object()
+        return post.author == user
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
