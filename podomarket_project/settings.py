8bdb32442bbc6d9f54b8068b1e2ec3203fa764c8
us3754 <useong3754@gmail.com>
2022-10-07 21:27:11 +0900

    이메일 설정
    이메일 전송이 실제로 되도록 작성

--- podomarket_project/settings.py
+++ podomarket_project/settings.py
@@ -154,5 +154,9 @@ ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = 'account_email_confirmation_
 
 # Email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "useong3754@gmail.com"
+EMAIL_HOST_PASSWORD = "uwjwusgivjqhwfxt"
