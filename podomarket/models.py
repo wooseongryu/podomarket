15c0b4b032b6ace855d75358e939ad0256f021e9
us3754 <useong3754@gmail.com>
2022-10-09 17:04:50 +0900

    댓글 모델 구현
    comment모델 작성
    User-Comment, Post-Comment 가 1:N 관계가 되도록 외래키 지정
    마이그레이션 생성 후 적용

--- podomarket/models.py
+++ podomarket/models.py
@@ -57,3 +57,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    content = models.TextField(max_length=500, blank=False)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:30]
+
