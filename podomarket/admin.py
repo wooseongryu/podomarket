d854619e166326fb38420f5f11d85585c332d545
us3754 <useong3754@gmail.com>
2022-10-10 23:39:56 +0900

    Inline
    유저 수정 페이지에서 유저를 팔로우 하는 유저들을 관리
    게시글 수정 페이지에서 게시글에 속한 댓글과 좋아요 관리
    댓글 수정 페이지에서 댓글에 속한 좋아요 관리

--- podomarket/admin.py
+++ podomarket/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Post
+from django.contrib.contenttypes.admin import GenericStackedInline
+from .models import User, Post, Comment, Like
+
+
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "kakao_id", "address", "profile_pic", 'following',)}),)
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+class LikeInline(GenericStackedInline):
+    model = Like
+
+class UserInline(admin.StackedInline):
+    model = User.following.through
+    fk_name = 'to_user'
+    verbose_name = "Follower"
+    verbose_name_plural = "Followers"
+
+UserAdmin.inlines = (
+    UserInline,
+)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
 
 admin.site.register(User, UserAdmin)
-UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "kakao_id", "address", "profile_pic",)}),)
-admin.site.register(Post)
+
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment, CommentAdmin)
+
+admin.site.register(Like)
